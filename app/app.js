a8b475347510af07fa95aecc5ba2ef5c1a72189b
unknown <thinkhong99@naver.com>
2021-04-28 17:17:06 +0900

    프론트엔드 구성

---
+++ app/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const home = require("./src/routes/home");
+app = express();
+
+
+app.set("views", `${__dirname}/src/views`);
+app.set("view engine", "ejs");
+
+
+app.use(express.static(`${__dirname}/src/public`))
+app.use("/", home);
+
+
+module.exports = app;
